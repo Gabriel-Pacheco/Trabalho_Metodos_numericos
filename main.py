3a9dc72cf701b46b7f8851431157b306e104af0a
Lipao12 <f.nunesmai19@gmail.com>
2022-07-26 09:52:50 -0300

    criando imports

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("fodase, Lucas")
+import scipy
+import scipy.linalg
+import scipy.sparse
+import scipy.sparse.linalg
+import numpy as np
+
+def main():
+   pass
+if __name__=="__main__":
+    main()
