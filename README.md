78f039bbec24a48e22e2d421325a3e3c1ef962b7
Lipao12 <f.nunesmai19@gmail.com>
2022-07-28 16:00:46 -0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Trabalho Metodos Numericos
-Ainda estamos esperando o professor liberar o que deve ser modelado 
+Modelar a temperatura de uma placa que possui 96 pontos internos, sendo 8 linhas e 12 colunas.
+
+![alt text](C:\Users\patri\Documents\GitHub\Trabalho_Metodos_numericos\imagem_placa.png)
--- main.py
+++ main.py
@@ -1,37 +1,25 @@
+'''
+Trabalho 1 - Algorítmos numéricos
+
+Por: Filipe Mai, Gabriel Pacheco e Lucas Tersari
+'''
+
 import scipy
 import scipy.linalg
 import scipy.sparse
 import scipy.sparse.linalg
 import numpy as np
 
-def showImage(X, Y, T, x, y):
-    import matplotlib;
-    matplotlib.use('Qt4Agg')
-    import matplotlib.pylab as plt
-    from mpl_toolkits.mplot3d import Axes3D
-    from matplotlib import cm
-    from matplotlib.ticker import LinearLocator, FormatStrFormatter
-    import numpy as np
-
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(X, Y, T, rstride=1, cstride=1, cmap=cm.coolwarm,
-                           linewidth=0, antialiased=False)
-    ax.set_zlim(0, 110)
-
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('T [$^o$C]')
-    ax.set_xticks(x)
-    ax.set_yticks(y)
-
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.show()
+def showImage(X, Y, Z, x, y):
+    pass
 
 def main():
-   pass
+   n = 96
+   gratterDiagonal = np.ones(n)
+   
+   # Creating the diagonals
+   d0 = 20*gratterDiagonal
+   d1 = 
 
 if __name__=="__main__":
     main()
